773d3a27ba5b395a54bdb13ab6022d2bb8d2a9c5
Say my name <vuphamlethanh.public@gmail.com>
2023-05-29 04:07:51 +0700

    feat<components>: create `Logo`

---
+++ src/views/components/data_displays/Logo.tsx
@@ -0,0 +1,11 @@
+function Logo()
+{
+   return (
+      <article  className={"logo"}>
+         <h1 className={"logo--primary"}>HalloHolla.com</h1>
+         <p className={"logo--secondary"}>Cable Management</p>
+      </article>
+   )
+}
+
+export default Logo
