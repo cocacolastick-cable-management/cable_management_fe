93c2bebf1297c4acd73f2663239afeea16134cef
Say my name <vuphamlethanh.public@gmail.com>
2023-05-29 02:54:39 +0700

    feat: init `tailwindcss`

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import reportWebVitals from './reportWebVitals'
+import "./views/styles/_index.scss"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
