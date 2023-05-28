cc5b75a083e9dbdb71ee156fb4056730419b4dfe
Say my name <vuphamlethanh.public@gmail.com>
2023-05-29 03:04:37 +0700

    feat<layouts>: create `RootLayout` and its style

---
+++ src/views/layouts/root/RootLayout.tsx
@@ -0,0 +1,12 @@
+import {Outlet} from "react-router-dom"
+
+function RootLayout()
+{
+   return (
+      <div className={"root-layout"}>
+         <Outlet/>
+      </div>
+   )
+}
+
+export default RootLayout
