da2e5805bae3e62237dd56f629555b70973d81af
Say my name <vuphamlethanh.public@gmail.com>
2023-05-29 03:26:37 +0700

    feat

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 import "./views/styles/_index.scss"
+import {RootRouter} from "./routers"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    <RootRouter/>
   </React.StrictMode>
 )
 
