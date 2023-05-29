60c3619b28e7ca3dd71071a742013987436e2126
Say my name <vuphamlethanh.public@gmail.com>
2023-05-29 17:58:25 +0700

    feat<views>: create `UserInfo`

---
+++ src/views/layouts/dashborad/UserInfo.tsx
@@ -0,0 +1,19 @@
+import {Chip} from '@mui/material'
+
+function UserInfo()
+{
+   return (
+      <article className={"user-info"}>
+         <div className={"user-info--top"}>
+            <p className={"user-info--top__name"}>Vu Planner</p>
+            <Chip
+               className={"user-info--top__role"} label="Admin"
+               color={"info"} variant="outlined" size={"small"}
+            />
+         </div>
+         <p className={"user-info__email"}>vuphamlethanh@gmail.com</p>
+      </article>
+   )
+}
+
+export default UserInfo
