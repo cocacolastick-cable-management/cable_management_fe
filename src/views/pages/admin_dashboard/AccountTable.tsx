3b2d13ec417212e2cbb403e6868e1188e2d33f82
Say my name <vuphamlethanh.public@gmail.com>
2023-05-31 05:28:17 +0700

    feat<views>: create `AccountTable`

---
+++ src/views/pages/admin_dashboard/AccountTable.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import {DataGrid, GridColDef, GridRowsProp} from '@mui/x-data-grid'
+import ToolBar from "./AccountTableToolBar"
+import { v4 as uuidv4 } from 'uuid';
+
+const rows: GridRowsProp = [
+   {id: uuidv4(), Serial: 1, Role: "xin chao 1", Name: "ok", Email: "ok@gmail.com"},
+   {id: uuidv4(), Serial: 1, Role: "xin chao 2", Name: "ok", Email: "ok@gmail.com"},
+   {id: uuidv4(), Serial: 1, Role: "xin chao 3", Name: "ok", Email: "ok@gmail.com"},
+   {id: uuidv4(), Serial: 1, Role: "xin chao 4", Name: "ok", Email: "ok@gmail.com"},
+   {id: uuidv4(), Serial: 1, Role: "xin chao 5", Name: "ok", Email: "ok@gmail.com"},
+   {id: uuidv4(), Serial: 1, Role: "xin chao 6", Name: "ok", Email: "ok@gmail.com"},
+   {id: uuidv4(), Serial: 1, Role: "xin chao 7", Name: "ok", Email: "ok@gmail.com"},
+];
+
+const columns: GridColDef[] = [
+   { field: 'Serial', headerName: 'Serial', flex: 0.1},
+   { field: 'Role', headerName: 'Role', flex: 0.15},
+   { field: 'Name', headerName: 'Name', flex: 0.3},
+   { field: 'Email', headerName: 'Email', flex: 0.45},
+];
+
+function AccountTable()
+{
+   return (
+      <div className={"account-table"}>
+         <DataGrid density={"standard"} rows={rows} columns={columns} slots={{ toolbar: ToolBar }}/>
+      </div>
+   );
+}
+
+export default AccountTable
