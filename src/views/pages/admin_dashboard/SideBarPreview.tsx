0e92e5dad4ec9e8ed3983d0b9f60514c78ee10f7
Say my name <vuphamlethanh.public@gmail.com>
2023-05-31 05:29:21 +0700

    feat<views>: create `SideBarPreview`

---
+++ src/views/pages/admin_dashboard/SideBarPreview.tsx
@@ -0,0 +1,11 @@
+function SideBarPreview()
+{
+   return (
+      <aside className={"side-bar-preview"}>
+         <p className={"text-2xl font-bold text-slate-400 mb-1"}>Nothing to show</p>
+         <p className={"text-slate-500"}>select something to see detail</p>
+      </aside>
+   )
+}
+
+export default SideBarPreview
