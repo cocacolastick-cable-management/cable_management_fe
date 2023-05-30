4bea0fa7636cbdbc1dcf9ac060927ca2a6c1d0a2
Say my name <vuphamlethanh.public@gmail.com>
2023-05-31 06:28:21 +0700

    Update SignInForm.tsx

--- src/views/pages/sign_in/SignInForm.tsx
+++ src/views/pages/sign_in/SignInForm.tsx
@@ -1,7 +1,7 @@
-import {Button, TextField, Chip, MenuItem} from "@mui/material"
+import {Button, TextField, Chip, MenuItem, SelectChangeEvent} from "@mui/material"
 import {Select} from "../../base_components"
 import {Roles} from "../../../constants"
-import {useCallback} from "react"
+import {useCallback, ChangeEvent} from "react"
 import {useFormik} from "formik"
 import {SignInRequest, SignInRequestValidation} from "../../../api_schema"
 
@@ -21,7 +21,7 @@ function SignInForm()
          console.log(values)
       }
    })
-   const { values, touched, errors, handleChange, handleSubmit, handleBlur } = form;
+   const { values, touched, errors, handleSubmit, handleBlur } = form;
 
    const renderRoleSelect = useCallback(() => {
       return roleSelectData.map((option, index) => (
@@ -34,25 +34,30 @@ function SignInForm()
       ))
    }, [])
 
+   const handleInputChange = (e: ChangeEvent<any> | SelectChangeEvent) => {
+      form.setFieldError(e.target.name, "")
+      form.handleChange(e)
+   };
+
    return (
       <form className={"sign-in-form"}>
          <Select
             error={!!(touched.Role && errors.Role)}
             helperText={touched.Role && errors.Role}
             name={"Role"} value={values.Role}
-            onChange={handleChange} onBlur={handleBlur} renderOptions={renderRoleSelect}
+            onChange={handleInputChange} onBlur={handleBlur} renderOptions={renderRoleSelect}
             label={"Role"} required/>
          <TextField
             error={!!(touched.Email && errors.Email)}
             helperText={touched.Email && errors.Email}
             name={"Email"} value={values.Email}
-            onBlur={handleBlur} onChange={handleChange} label={"Email"}
+            onBlur={handleBlur} onChange={handleInputChange} label={"Email"}
             type={"email"} required/>
          <TextField
             error={!!(touched.Password && errors.Password)}
             helperText={touched.Password && errors.Password}
             name={"Password"} value={values.Password}
-            onBlur={handleBlur} onChange={handleChange} label={"Password"} required/>
+            onBlur={handleBlur} onChange={handleInputChange} label={"Password"} required/>
          <Button
             onClick={() => handleSubmit()}
             variant={"contained"} size={"large"}>
