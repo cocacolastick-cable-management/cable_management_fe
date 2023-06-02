a67d10f953a1ff680e692f2c3b05dfb2b5990b42
Say my name <vuphamlethanh.public@gmail.com>
2023-06-03 02:56:01 +0700

    feat<views>: create `WithDrawTable`

---
+++ src/views/pages/planner_dashboard/with_draw/WithDrawTable.tsx
@@ -0,0 +1,58 @@
+import {DataGrid, GridColDef} from "@mui/x-data-grid";
+import {RootState, useRootDispatch} from "../../../../stores/RootStore";
+import {useSelector} from "react-redux";
+import {useEffect, useMemo} from "react";
+import {fetchPlannerWithDrawList} from "../../../../stores/WithDrawTableStore";
+import {GridRowSelectionModel} from "@mui/x-data-grid/models/gridRowSelectionModel";
+import WithDrawTableToolBar from "./WithDrawTableToolBar";
+import formatFriendlyDatetime from "../../../../utils/formatFriendlyDatetime";
+
+const columns: GridColDef[] = [
+   { field: 'Serial', headerName: 'Serial', flex: 0.05},
+   { field: 'Status', headerName: 'Status', flex: 0.1},
+   { field: 'CableAmount', headerName: 'CableAmount', flex: 0.1},
+   { field: 'SupplierEmail', headerName: 'SupplierEmail', flex: 0.25},
+   { field: 'ContractorEmail', headerName: 'ContractorEmail', flex: 0.25},
+   { field: 'CreatedAt', headerName: 'CreatedAt', flex: 0.25},
+];
+
+function WithDrawTable()
+{
+   const dispatch = useRootDispatch()
+   const {withDrawList, status} = useSelector((state: RootState) => state.WithDrawTableSlice)
+
+   useEffect(() => {
+      if (withDrawList === null) dispatch(fetchPlannerWithDrawList())
+   }, [dispatch, withDrawList])
+
+   const rows = useMemo(() => {
+      return withDrawList?.map((withDraw, index) => {
+         return {
+            id: withDraw.Id,
+            Serial: index + 1,
+            Status: withDraw.Status,
+            CableAmount: withDraw.CableAmount,
+            SupplierEmail: withDraw.SupplierEmail,
+            ContractorEmail: withDraw.ContractorEmail,
+            CreatedAt: formatFriendlyDatetime(withDraw.CreatedAt)
+         }
+      })
+   }, [withDrawList])
+   
+   const handleRowSelectionChange = (rowSelectionModel: GridRowSelectionModel) => {
+      // dispatch(setSelectedUser({id: rowSelectionModel[0] as string}))
+   }
+
+   return (
+      <div className={"with-draw-table"}>
+         <DataGrid
+            loading={status === "pending"}
+            onRowSelectionModelChange={handleRowSelectionChange}
+            density={"standard"} columns={columns} rows={rows ?? []}
+            slots={{ toolbar: WithDrawTableToolBar }}
+         />
+      </div>
+   )
+}
+
+export default WithDrawTable
