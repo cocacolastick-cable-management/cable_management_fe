921ce63dbb04ee35ebf57b3e49d82906cf021db7
Say my name <vuphamlethanh.public@gmail.com>
2023-06-03 02:48:55 +0700

    feat<api_schema>: create `WithDrawHistoryResponse`

---
+++ src/api_schema/responses/WithDrawHistoryResponse.ts
@@ -0,0 +1,22 @@
+type WithDrawHistoryResponse = {
+   CreatorId: string
+   CreatorName: string
+   CreatorEmail: string
+   CreatorRole: string
+   CreatedAt: Date
+   Status: string
+   Action: string
+}
+
+// type HistoryResponse struct {
+//    CreatorId    uuid.UUID
+//    CreatorName  string
+//    CreatorEmail string
+//    CreatorRole  string
+//    CreatedAt    time.Time
+//    Status       string
+//    Action       string
+// }
+
+
+export default WithDrawHistoryResponse
