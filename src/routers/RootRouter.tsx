ce5a5e7844dc61b49efe3e39914d6885b7e90d09
Say my name <vuphamlethanh.public@gmail.com>
2023-06-03 02:49:33 +0700

    Update RootRouter.tsx

--- src/routers/RootRouter.tsx
+++ src/routers/RootRouter.tsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
-import {AdminDashBoardPage, DashBoardLayout, RootLayout, SignInPage} from "../views"
+import {AdminDashBoardPage, DashBoardLayout, PrivateRouteLayout, RootLayout, SignInPage, PlannerDashBoardPage} from "../views"
 
 function RootRouter()
 {
@@ -8,23 +8,27 @@ function RootRouter()
 
          <Routes>
 
-            <Route path={"/"} element={<RootLayout/>}>
+            <Route element={<RootLayout/>}>
 
                <Route path={"sign-in"} element={<SignInPage/>}/>
 
-               <Route path={"admin"} element={<DashBoardLayout/>}>
-                  <Route path={"dashboard"} element={<AdminDashBoardPage/>} />
+               <Route element={<PrivateRouteLayout/>}>
+
+                  <Route path={"admin"} element={<DashBoardLayout/>}>
+                     <Route path={"dashboard"} element={<AdminDashBoardPage/>} />
+                  </Route>
+
+                  <Route path={"planner"} element={<DashBoardLayout/>}>
+                     <Route path={"dashboard"} element={<PlannerDashBoardPage/>} />
+                  </Route>
+
+                  <Route path={"supplier"} element={<DashBoardLayout/>}>
+
+                  </Route>
+
+                  <Route path={"contractor"} element={<DashBoardLayout/>}>
+
                </Route>
-
-               <Route path={"planner"} element={<DashBoardLayout/>}>
-
-               </Route>
-
-               <Route path={"supplier"} element={<DashBoardLayout/>}>
-
-               </Route>
-
-               <Route path={"contractor"} element={<DashBoardLayout/>}>
 
                </Route>
 
