822a3ae7af09d9494349bf9db80c1540b21f3aa1
Say my name <vuphamlethanh.public@gmail.com>
2023-06-03 02:53:00 +0700

    Update UserInfo.tsx

--- src/views/layouts/dashborad/UserInfo.tsx
+++ src/views/layouts/dashborad/UserInfo.tsx
@@ -1,17 +1,30 @@
 import {Chip} from '@mui/material'
+import {Roles} from "../../../constants"
+import {useSelector} from "react-redux"
+import {RootState} from "../../../stores/RootStore"
+import {Navigate} from "react-router-dom"
 
 function UserInfo()
 {
+   const authData = useSelector((state: RootState) => state.AuthSlice.authData)
+   const role = Roles[authData.Role as keyof typeof Roles]
+
+   if (role === undefined) {
+      return <Navigate to={"/sign-in"} replace/>
+   }
+
    return (
       <article className={"user-info"}>
          <div className={"user-info--top"}>
-            <p className={"user-info--top__name"}>Vu Planner</p>
+            <p className={"user-info--top__name"}>{authData.Name}</p>
             <Chip
-               className={"user-info--top__role"} label="Admin"
-               color={"primary"} variant="outlined" size={"small"}
+               className={"user-info--top__role"}
+               label={role.value}
+               color={role.color}
+               variant="outlined" size={"small"}
             />
          </div>
-         <p className={"user-info__email"}>vuphamlethanh@gmail.com</p>
+         <p className={"user-info__email"}>{authData.Email}</p>
       </article>
    )
 }
