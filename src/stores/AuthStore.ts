cceadcf20ec8caa7c4c09592c87cc102b97d87e7
Say my name <vuphamlethanh.public@gmail.com>
2023-06-03 02:49:53 +0700

    feat<stores>: create `AuthStore`

---
+++ src/stores/AuthStore.ts
@@ -0,0 +1,46 @@
+import {AuthResponse} from "../api_schema";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface AuthStateType {
+   authData: AuthResponse
+   isAuth: boolean
+}
+
+const localAuthDataKey = "localAuthDataKey"
+
+const authData: AuthResponse = JSON.parse(localStorage.getItem(localAuthDataKey)!)
+
+const initialState: AuthStateType = {
+   authData: authData ?? {
+      Role: null,
+      Name: "",
+      Email: "",
+      AccessToken: "",
+      RefreshToken: ""
+   },
+   isAuth: !!authData
+}
+
+const AuthSlice = createSlice({
+   name: "AuthSlice",
+   initialState,
+   reducers: {
+      setAuthData: (state, action: PayloadAction<AuthResponse>) => {
+         state.authData = action.payload
+         localStorage.setItem(localAuthDataKey, JSON.stringify(action.payload))
+      },
+      clearAuthData: (state) => {
+         state.authData = initialState.authData
+         state.authData = initialState.authData
+      },
+      fetchAuthDataFromLocal: (state) => {
+         state.authData = JSON.parse(localStorage.getItem(localAuthDataKey)!) as AuthResponse
+      }
+   }
+})
+
+export {AuthSlice, localAuthDataKey}
+export const {
+   setAuthData, clearAuthData, fetchAuthDataFromLocal
+} = AuthSlice.actions
+export default AuthSlice.reducer
