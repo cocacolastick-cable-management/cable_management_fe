530bfab9fe16ac79476388431c79c86edc71624a
Say my name <vuphamlethanh.public@gmail.com>
2023-06-03 02:50:17 +0700

    feat<stores>: create `SupplierTableStore`

---
+++ src/stores/SupplierTableStore.ts
@@ -0,0 +1,15 @@
+// import {ContractResponse} from "../api_schema";
+//
+// interface ContractTableStateType {
+//    contractList: ContractResponse[] | null
+//    selectedWithDraw: ContractResponse | null
+//    status: 'idle' | 'pending' | 'succeeded' | 'failed'
+// }
+//
+// const initialState: ContractTableStateType = {
+//    contractList: null,
+//    selectedWithDraw: null,
+//    status: 'idle'
+// }
+
+export {}
