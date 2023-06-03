a74e58ea94d041bc9ba5ca0ca48d3514f1a234b3
Say my name <vuphamlethanh.public@gmail.com>
2023-06-03 20:39:37 +0700

    feat<views>: add `sideBar` to `TableLayout`

--- src/views/layouts/table/TableLayout.tsx
+++ src/views/layouts/table/TableLayout.tsx
@@ -1,11 +1,10 @@
-import {IconButton} from "@mui/material";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import SideBarPreview from "./SideBarPreview";
 import {ReactNode} from "react";
 
 type TableLayoutProps = {
    name?: string
    table?: ReactNode
+   sideBar?: ReactNode
 }
 
 function TableLayout(props: TableLayoutProps)
@@ -13,11 +12,11 @@ function TableLayout(props: TableLayoutProps)
    return (
       <section className={"table-layout"}>
          <header className={"table-layout__header"}>
-            <IconButton size={"small"}><ArrowBackIosIcon fontSize={"small"}/></IconButton>
+            {/*<IconButton size={"small"}><ArrowBackIosIcon fontSize={"small"}/></IconButton>*/}
             <p className={"table-layout__header__title"}>{props.name}</p>
          </header>
          <div className={"table-layout__body"}>
-            <SideBarPreview/>
+            <SideBarPreview element={props.sideBar}/>
             <div className={"table-layout__body--main"}>{props.table}</div>
          </div>
       </section>
