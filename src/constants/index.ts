0c887b3e0a5d78b141fd137573d34f6f70d35117
Say my name <vuphamlethanh.public@gmail.com>
2023-06-06 14:53:44 +0700

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,13 @@
 import Roles from "./Roles"
 import WithDrawStatus, {KeyOfWithDrawStatus} from "./WithDrawStatus"
+import NotifTypes, {NotifTypesType} from "./NotifTypes"
 
 export {
    Roles,
 
    WithDrawStatus,
-   type KeyOfWithDrawStatus
+   type KeyOfWithDrawStatus,
+
+   NotifTypes,
+   type NotifTypesType
 }
