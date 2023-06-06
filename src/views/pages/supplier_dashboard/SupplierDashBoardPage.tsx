acdecfb5b4d624e9ddaf44f8de7da97f0d1e4d4e
Say my name <vuphamlethanh.public@gmail.com>
2023-06-06 14:50:01 +0700

    Create SupplierDashBoardPage.tsx

---
+++ src/views/pages/supplier_dashboard/SupplierDashBoardPage.tsx
@@ -0,0 +1,51 @@
+import {TableLayout} from "../../layouts/table";
+import {ReactNode, useEffect, useState} from "react";
+import WithDrawPreview from "./WithDrawPreview";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../../stores/RootStore";
+import {TabNav, TabRouteType} from "../../base_components";
+import NotifSection from "../planner_dashboard/NotifSection";
+import {ContractTable, WithDrawTable} from "../../components";
+import {setSelectedContract} from "../../../stores/PlannerDashBoardStore";
+
+function SupplierDashBoardPage()
+{
+   const {selectedWithDraw} = useSelector((state: RootState) => state.PlannerDashBoardSlice)
+
+   const [previewUI, setPreviewUI] = useState<ReactNode>(null)
+
+   useEffect(() => {
+      console.log(setSelectedContract)
+      if (selectedWithDraw) setPreviewUI(<WithDrawPreview data={selectedWithDraw}/>)
+      else setPreviewUI(null)
+   }, [selectedWithDraw])
+
+   return (
+      <TableLayout
+         sideBar={previewUI}
+         name={"SUPPLIER DASHBOARD"}
+         table={<TabNav routes={routes}/>}
+      />
+   )
+}
+
+const routes: TabRouteType[] = [
+   {
+      label: "NOTIFICATIONS",
+      element: <NotifSection/>
+      // isClosable: true
+   },
+   {
+      label: "WITH DRAW REQUESTS",
+      element: <WithDrawTable/>,
+      // isClosable:
+   },
+   {
+      label: "CONTRACTS",
+      element: <ContractTable/>,
+      // isClosable:
+   }
+]
+
+
+export default SupplierDashBoardPage
