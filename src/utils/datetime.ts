9bb1c70966a4e7f60b2e36cb5362b0f973959764
Say my name <vuphamlethanh.public@gmail.com>
2023-06-06 14:51:05 +0700

    Update datetime.ts

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -21,4 +21,16 @@ function formatFriendlyDatetime(datetime: Date): string
    }
 }
 
-export {formatFriendlyDatetime}
+function formatDatetime(datetime: Date): string
+{
+   const date = new Date(datetime);
+   const year = date.getFullYear().toString().slice(2);
+   const month = (date.getMonth() + 1).toString().padStart(2, "0");
+   const day = date.getDate().toString().padStart(2, "0");
+   const hours = date.getHours().toString().padStart(2, "0");
+   const minutes = date.getMinutes().toString().padStart(2, "0");
+
+   return `${year}-${month}-${day} ${hours}:${minutes}`
+}
+
+export {formatFriendlyDatetime, formatDatetime}
