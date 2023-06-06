a9a8f748fc99336543b98eeece9b29037b23a0fb
Say my name <vuphamlethanh.public@gmail.com>
2023-06-06 14:50:43 +0700

    Update index.ts

--- src/views/components/data_displays/index.ts
+++ src/views/components/data_displays/index.ts
@@ -1,7 +1,13 @@
 import Logo from "./Logo"
 import UserTable from "./UserTable"
+import HistoryItem from "./HistoryItem"
+import ContractTable from "./ContractTable"
+import WithDrawTable from "./WithDrawTable"
 
 export {
    Logo,
-   UserTable
+   UserTable,
+   HistoryItem,
+   ContractTable,
+   WithDrawTable
 }
