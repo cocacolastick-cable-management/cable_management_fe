b8b1aac32e9ebaa7245fe2803b0c9bd113ac9e2c
Say my name <vuphamlethanh.public@gmail.com>
2023-06-06 14:51:35 +0700

    Update index.ts

--- src/api_schema/requests/index.ts
+++ src/api_schema/requests/index.ts
@@ -1,10 +1,17 @@
 import SignInRequest, {SignInRequestValidation} from "./SignInRequest"
 import CreateUserRequest, {CreateUserRequestValidation} from "./CreateUserRequest"
+import CreateWithDrawRequest, {CreateWithDrawRequestValidation} from "./CreateWithDrawRequest"
+import UpdateWithDrawStatusRequest from "./UpdateWithDrawStatusRequest"
 
 export {
    type SignInRequest,
    SignInRequestValidation,
 
    type CreateUserRequest,
-   CreateUserRequestValidation
+   CreateUserRequestValidation,
+
+   type CreateWithDrawRequest,
+   CreateWithDrawRequestValidation,
+
+   type UpdateWithDrawStatusRequest
 }
