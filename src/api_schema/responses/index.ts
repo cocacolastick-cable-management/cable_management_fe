2b25e1a9a8c3b6a259820ea0131fac223828c9e3
Say my name <vuphamlethanh.public@gmail.com>
2023-06-06 14:53:49 +0700

    Update index.ts

--- src/api_schema/responses/index.ts
+++ src/api_schema/responses/index.ts
@@ -3,11 +3,13 @@ import AuthResponse from "./AuthResponse"
 import WithDrawResponse from "./WithDrawResponse"
 import WithDrawHistoryResponse from "./WithDrawHistoryResponse"
 import ContractResponse from "./ContractResponse"
+import NotifResponse from "./NotifResponse"
 
 export {
    type UserResponse,
    type AuthResponse,
    type WithDrawResponse,
    type WithDrawHistoryResponse,
-   type ContractResponse
+   type ContractResponse,
+   type NotifResponse
 }
